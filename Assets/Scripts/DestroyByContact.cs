f5aecd9a1d5dd28ee7cde535a474e977aede310a
Lorenzo Battiston <lorenzo.battiston@r132-04-w10.SCHULE-ISERV>
2017-11-02 14:00:15 +0100

    first commit

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+	public GameObject Explosions;
+	public GameObject playerExplosions;
+	public int ScoreValue;
+	public GameController gameController;
+
+	void Start ()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+	}
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Boundary")
+		{
+			return;
+		}
+			Instantiate (Explosions, transform.position, transform.rotation);
+		if (other.tag == "Player") {
+			Instantiate (playerExplosions, other.transform.position, other.transform.rotation);
+		}
+		gameController.AddScore (ScoreValue);
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+}
